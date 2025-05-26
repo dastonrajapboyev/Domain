378013f69615c008786108ac7faddbc4a35a3a05
dastonrajapboyev <dastonrajapboyev79@gmail.com>
2025-05-26 23:36:54 +0500

    Landing page qilindi

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,7 +3,7 @@ import Hero from "../components/Hero";
 import Features from "../components/Features";
 // import CTA from "../components/CTA";
 import Footer from "../components/Footer";
-import CTA from "../components/Cta";
+import CTA from "../components/CTA";
 import Stats from "../components/Stats";
 import CharacteristicsSection from "../components/CharakteristikSection";
 import CTASection from "../components/CTASection";
